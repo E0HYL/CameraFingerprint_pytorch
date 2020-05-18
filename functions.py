14db94db163a0886cded3fc108cafcaeba7c4350
E0HYL <heyilinge0@gmail.com>
2020-05-18 18:26:33 +0800

    del header from modified file

--- functions.py
+++ functions.py
@@ -1,25 +1,10 @@
-"""
-Functions implementing custom NN layers
-
-Copyright (C) 2018, Matias Tassano <matias.tassano@parisdescartes.fr>
-
-This program is free software: you can use, modify and/or
-redistribute it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later
-version. You should have received a copy of this license along
-this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 import torch
 from torch.autograd import Function, Variable
 
 def concatenate_input_noise_map(input, noise_sigma):
 	r"""Implements the first layer of FFDNet. This function returns a
 	torch.autograd.Variable composed of the concatenation of the downsampled
-	input image and the noise map. Each image of the batch of size CxHxW gets
-	converted to an array of size 4*CxH/2xW/2. Each of the pixels of the
-	non-overlapped 2x2 patches of the input image are placed in the new array
-	along the first dimension.
+	input image and the noise map.
 
 	Args:
 		input: batch containing CxHxW images
--- models.py
+++ models.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""
-Definition of the FFDNet model and its custom layers
-
-Copyright (C) 2018, Matias Tassano <matias.tassano@parisdescartes.fr>
-
-This program is free software: you can use, modify and/or
-redistribute it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later
-version. You should have received a copy of this license along
-this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
